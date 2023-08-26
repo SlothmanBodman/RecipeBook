9e8623a44b2c21621f1ce1b612df551dde188b74
Jack Bodman <jack.bodman1998@outlook.com>
2023-08-26 18:02:28 +0100

    Migrations

---
+++ recipe-book/database/migrations/2023_08_26_145759_create_recipe_ingredients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('recipe_id');
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('restrict');
+            $table->string('quantity_one')->nullable();
+            $table->string('quantity_two')->nullable();
+            $table->string('quantity_three')->nullable();
+            $table->string('quantity_four')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_ingredients');
+    }
+};
